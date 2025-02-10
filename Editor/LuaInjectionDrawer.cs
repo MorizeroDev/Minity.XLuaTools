9ce16dba355c7a652e3d9eb833b6edcfb819ec4f
Buger404 <ris_vb@126.com>
2025-02-10 16:39:40 +0800

    chore: first commit

---
+++ Editor/LuaInjectionDrawer.cs
@@ -0,0 +1,73 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Minity.XLuaTools.Editor
+{
+    [CustomPropertyDrawer(typeof(LuaBehaviour.Injection))]
+    public class LuaInjectionDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            var container = new VisualElement
+            {
+                style =
+                {
+                    flexDirection = FlexDirection.Row,
+                    justifyContent = Justify.SpaceBetween,
+                }
+            };
+
+
+
+            var objField = new PropertyField(property.FindPropertyRelative("Object"), "")
+            {
+                style =
+                {
+                    width = new StyleLength(Length.Percent(30f))
+                }
+            };
+            container.Add(objField);
+            
+            
+            var obj = (GameObject)property.FindPropertyRelative("Object").objectReferenceValue;
+
+            if (obj)
+            {
+                var components = new List<Component>();
+                obj.GetComponents(components);
+                var types = components.Select(x => x.GetType().Name).Distinct().ToList();
+                types.Insert(0, "GameObject");
+                var typeProp = property.FindPropertyRelative("Type");
+                
+                var nameField = new PropertyField(property.FindPropertyRelative("Name"), "")            
+                {
+                    style =
+                    {
+                        width = new StyleLength(Length.Percent(30f))
+                    }
+                };
+                var typeField = new DropdownField(types, 
+                    Math.Max(0, types.FindIndex(x => x == typeProp.stringValue)))
+                {
+                    style =
+                    {
+                        width = new StyleLength(Length.Percent(40f))
+                    }
+                };
+                typeField.BindProperty(typeProp);
+            
+                container.Add(nameField);
+                container.Add(typeField);
+            }
+            
+            return container;
+        }
+    }
+}
+#endif
---
+++ LuaEnvGuard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Minity.Infra;
+using UnityEngine;
+using XLua;
+
+namespace Minity.XLuaTools
+{
+    public class LuaEnvGuard : GlobalSingleton<LuaEnvGuard>
+    {
+        public static readonly LuaEnv Environment = new();
+
+        private const float GC_INTERVAL = 1f;
+
+        private float lastGCTime;
+        
+        private void Update()
+        {
+            if (Time.unscaledTime - lastGCTime >= GC_INTERVAL)
+            {
+                lastGCTime = Time.unscaledTime;
+                Environment.Tick();
+            }
+        }
+    }
+}
